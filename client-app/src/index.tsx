42fc862ae150b858c92e24a828778863ee275d76
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-09-26 17:39:49 -0500

    Styling dummy

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
+import "react-calendar/dist/Calendar.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,7 +12,7 @@ ReactDOM.render(
   <StoreContext.Provider value={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter> 
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById("root")
 );
