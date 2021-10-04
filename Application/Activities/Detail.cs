a3a14b60ffb7f73e6b43402d3a7a5e3f5ea800a2
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-10-03 22:24:06 -0500

    Added validations, exception handling, custom error pages

--- Application/Activities/Detail.cs
+++ Application/Activities/Detail.cs
@@ -1,4 +1,5 @@
-﻿using Domain;
+﻿using Application.Core;
+using Domain;
 using MediatR;
 using Persistence;
 using System;
@@ -12,12 +13,12 @@ namespace Application.Activities
 {
     public class Detail
     {
-        public class Query : IRequest<Activity> 
+        public class Query : IRequest<Result<Activity>> 
         { 
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, Result<Activity>>
         {
             private readonly DataContext _context;
 
@@ -26,9 +27,9 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.FindAsync(request.Id);
+                return Result<Activity>.Success(await _context.Activities.FindAsync(request.Id));
             }
         }
     }
