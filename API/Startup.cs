8c2d7e411debb9234e0a8ee15ac7ce25177e0ad8
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-10-13 21:03:48 -0500

    Added user entity, added IdentityDbContext, Configured Identity in Startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,10 +34,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddFluentValidation(config => {
+            services.AddControllers().AddFluentValidation(config =>
+            {
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
