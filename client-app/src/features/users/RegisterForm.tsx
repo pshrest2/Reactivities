45a330dde9894707a7a13f36e4ad65d373b27492
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-10-17 16:41:34 -0500

    Added the register form

---
+++ client-app/src/features/users/RegisterForm.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Formik, Form, ErrorMessage } from "formik";
+import MyTextInput from "../../app/common/form/MyTextInput";
+import { Button, Header, Label } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import * as Yup from "yup";
+
+const RegisterForm = () => {
+  const { userStore } = useStore();
+
+  return (
+    <Formik
+      initialValues={{
+        displayName: "",
+        username: "",
+        email: "",
+        password: "",
+        error: null,
+      }}
+      onSubmit={(values, { setErrors }) =>
+        userStore
+          .register(values)
+          .catch((error) => setErrors({ error: "Invalid email or password" }))
+      }
+      validationSchema={Yup.object({
+        displayName: Yup.string().required(),
+        username: Yup.string().required(),
+        email: Yup.string().required().email(),
+        password: Yup.string().required(),
+      })}
+    >
+      {({ handleSubmit, isSubmitting, errors, isValid, dirty }: any) => (
+        <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+          <Header
+            as="h2"
+            content="Signup to Reactivities"
+            color="teal"
+            textAlign="center"
+          />
+          <MyTextInput name="displayName" placeholder="Display Name" />
+          <MyTextInput name="username" placeholder="Username" />
+          <MyTextInput name="email" placeholder="Email" />
+          <MyTextInput name="password" placeholder="Password" type="password" />
+          <ErrorMessage
+            name="error"
+            render={() => (
+              <Label
+                style={{ marginBottom: 10 }}
+                basic
+                color="red"
+                content={errors.error}
+              />
+            )}
+          />
+          <Button
+            disabled={!isValid || !dirty || isSubmitting}
+            loading={isSubmitting}
+            positive
+            content="Register"
+            type="submit"
+            fluid
+          />
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default observer(RegisterForm);
