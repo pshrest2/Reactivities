692e5011c3bf0737ed22b483d2b1b85aea43fc24
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-10-18 22:30:34 -0500

    Add custom mapping

--- Application/Activities/Detail.cs
+++ Application/Activities/Detail.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System;
 using System.Collections.Generic;
@@ -13,23 +16,29 @@ namespace Application.Activities
 {
     public class Detail
     {
-        public class Query : IRequest<Result<Activity>> 
-        { 
+        public class Query : IRequest<Result<ActivityDto>>
+        {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Result<Activity>>
+        public class Handler : IRequestHandler<Query, Result<ActivityDto>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<Activity>.Success(await _context.Activities.FindAsync(request.Id));
+                var activities = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                return Result<ActivityDto>.Success(activities);
             }
         }
     }
