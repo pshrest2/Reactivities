3796d06ece13482e19925a5de1e37b36cbe6938c
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-11-05 19:10:48 -0500

    adding vertical paging

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,27 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { observer } from "mobx-react-lite";
-import { Grid } from "semantic-ui-react";
+import { Button, Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import ActivityList from "./ActivityList";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityFilters from "./ActivityFilters";
+import { PagingParams } from "../../../app/models/pagination";
 
 const ActivityDashboard = () => {
   const { activityStore } = useStore();
-  const { loadActivities, activityRegistry, loadingInitial } = activityStore;
+  const {
+    loadActivities,
+    activityRegistry,
+    loadingInitial,
+    setPagingParams,
+    pagination,
+  } = activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  const handleGetNext = () => {
+    setLoadingNext(true);
+    setPagingParams(new PagingParams(pagination!.currentPage + 1));
+    loadActivities().then(() => setLoadingNext(false));
+  };
 
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
   }, [activityRegistry.size, loadActivities]);
 
-  if (loadingInitial)
+  if (loadingInitial && !loadingNext)
     return <LoadingComponent content="Loading activities..." />;
 
   return (
     <Grid>
       <Grid.Column width="10">
         <ActivityList />
+        <Button
+          float="rignt"
+          content="More..."
+          positive
+          onClick={handleGetNext}
+          loading={loadingNext}
+          disabled={pagination?.totalPages === pagination?.currentPage}
+        />
       </Grid.Column>
       <Grid.Column width="6">
         <ActivityFilters />
